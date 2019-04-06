3d4a9e2dc890e041272af416a35aa990541bd3bb
fares <fares.helmy93@gmail.com>
2019-04-06 18:37:11 +0200

    final changes

--- src/main/java/datasource/DataSourceCreator.java
+++ src/main/java/datasource/DataSourceCreator.java
@@ -9,6 +9,8 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,31 +24,11 @@ public class DataSourceCreator {
 
     //fares
     public static DataSource getMySqlDataSource() {
-        Properties properties = new Properties();
-        FileInputStream fileInputStream = null;
         MysqlDataSource mysqlDataSource = null;
-
-        try {
-            fileInputStream = new FileInputStream("db.properties");
-            properties.load(fileInputStream);
-            mysqlDataSource = new MysqlDataSource();
-            mysqlDataSource.setURL(properties.getProperty("MYSQL_DB_URL"));
-            mysqlDataSource.setUser(properties.getProperty("MYSQL_DB_USERNAME"));
-            mysqlDataSource.setPassword(properties.getProperty("MYSQL_DB_PASSWORD"));
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(DataSourceCreator.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(DataSourceCreator.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(DataSourceCreator.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
+        mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setURL("jdbc:mysql://localhost:3306/fares");
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("root");
         return mysqlDataSource;
     }
 }
